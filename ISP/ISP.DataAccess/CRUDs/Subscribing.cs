23b99f2e5d6b21ac08f1094c0fd7a4f9141a8dc9
eng.elaf <elaf.dabalouni@gmail.com>
2025-10-24 14:48:55 -0800

    Initial Subscribing CRUD and adding mapster library

---
+++ ISP/ISP.DataAccess/CRUDs/Subscribing.cs
@@ -0,0 +1,50 @@
+﻿using ISP.DataAccess.Models;
+using ISP.DataAccess.ModelsDTO;
+using Mapster;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISP.DataAccess.CRUDs
+{
+    public class SubscribingAccess
+    {
+
+        private ISPSystemContext _context = new ISPSystemContext();
+
+        //Add Data
+        public void AddSubscribe(Subscribing newSubscribe)
+        {
+            var subscribingDTO = newSubscribe.Adapt<SubscribingDTO>();
+            _context.Subscribings.Add(newSubscribe);
+            _context.SaveChanges();
+        }
+
+        //Read Data
+        public Subscribing GetSubscribe(int id)
+        {
+            var subscribe = _context.Subscribings.FirstOrDefault(s => s.Id == id);
+            return subscribe;
+        }
+
+        //Update Data
+        public void UpdateSubscribe(Subscribing newSubscribe, int id)
+        {
+            var subscribe = _context.Subscribings.FirstOrDefault(s => s.Id == id);
+            subscribe.DeliveredAt = newSubscribe.DeliveredAt;
+            _context.SaveChanges();
+        }
+
+
+        //Delete Data
+        public void DeleteSubscribe(int id)
+        {
+            var subscribe = _context.Subscribings.FirstOrDefault(s => s.Id == id);
+            subscribe.IsDeleted = true; //soft delete
+
+        }
+
+    }
+}
---
+++ ISP/ISP.DataAccess/ModelsDTO/SubscribingDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISP.DataAccess.ModelsDTO
+{
+    public class SubscribingDTO
+    {
+        public enum SubscribingStatus { Active, Stopped, Deleted }
+        public SubscribingStatus Status { get; set; }
+        public DateTime DeliveredAt { get; set; }
+        public DateTime RetrievedAt { get; set; }
+    }
+}
